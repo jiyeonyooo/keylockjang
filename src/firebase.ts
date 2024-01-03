74baf43384ede600be3357ca50fcb3e95dab1144
jiyeonyoo <ji04230423@gmail.com>
2024-01-03 23:31:10 +0900

    Setting firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDppLrB1BlG38qya9nuQQfW4DE6RRSTxtI",
+  authDomain: "keylockjang.firebaseapp.com",
+  projectId: "keylockjang",
+  storageBucket: "keylockjang.appspot.com",
+  messagingSenderId: "807403968740",
+  appId: "1:807403968740:web:153655e95468a4ffc8f4f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
